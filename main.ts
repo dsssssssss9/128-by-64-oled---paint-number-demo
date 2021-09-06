641e608b6949d63bda6c9b73572c754627639be6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-06 22:57:20 +0100

    add comments

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+// Display Posistion number ( 0-3) in matching posistion
 input.onButtonPressed(Button.A, function () {
+    kitronik_VIEW128x64.clear()
     for (let index = 0; index <= 3; index++) {
+        // character posistions from 0 - LHS to 3 - RHS
+        // 
         kitronik_VIEW128x64.drawnum(index, index)
         kitronik_VIEW128x64.refresh()
         basic.pause(200)
@@ -7,7 +11,11 @@ input.onButtonPressed(Button.A, function () {
         kitronik_VIEW128x64.refresh()
     }
 })
+// display each segment ( A - G )  in each of the 4 main posistions across the display
+// *NOTE* this is pixel posistion NOT Column number!
+// 
 input.onButtonPressed(Button.B, function () {
+    kitronik_VIEW128x64.clear()
     for (let index = 0; index <= 6; index++) {
         for (let index2 = 0; index2 <= 3; index2++) {
             kitronik_VIEW128x64.drawsegment(Segment_List[index], step_list[index2])
@@ -22,12 +30,17 @@ let Segment_List: string[] = []
 let step_list: number[] = []
 basic.showIcon(IconNames.Yes)
 kitronik_VIEW128x64.clear()
+// List of screen posistions ( Columns? )
+// 
 step_list = [
 0,
 32,
 64,
 96
 ]
+// List of 7-Segment Display Segment "Names"
+// 
+// https://en.wikipedia.org/wiki/Seven-segment_display#/media/File:7_Segment_Display_with_Labeled_Segments.svg
 Segment_List = [
 "A",
 "B",
@@ -37,6 +50,3 @@ Segment_List = [
 "F",
 "G"
 ]
-basic.forever(function () {
-	
-})
